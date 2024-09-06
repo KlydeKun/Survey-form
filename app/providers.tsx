8942dff8740aaa2426646cbc1b580e2bb61b054e
KlydeKun <kylelerio15@gmail.com>
2024-09-07 02:00:14 +0800

    Setting up the project

---
+++ app/providers.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+import { PrimeReactProvider } from "primereact/api";
+
+export function Providers({ children }: { children: React.ReactNode }) {
+  return <PrimeReactProvider>{children}</PrimeReactProvider>;
+}
