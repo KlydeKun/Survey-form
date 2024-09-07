9e44c13e0f2f7d1bfc616e253a2c87bef52b0523
KlydeKun <kylelerio15@gmail.com>
2024-09-07 23:35:56 +0800

    Setting up middleware

---
+++ middleware.ts
@@ -0,0 +1,15 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export default function middleware(request: NextRequest) {
+  const { pathname } = request.nextUrl;
+
+  if (pathname === "/") {
+    return NextResponse.redirect(new URL("/survey-form", request.url));
+  }
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/"],
+};
